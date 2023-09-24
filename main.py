f8dc8b3772510b2055d6322e3c3536858cd3f954
Timofey <timofey.brayko@gmail.com>
2023-09-25 00:59:29 +0300

    Changes input format and reading file implementation. Fix problem with pivot index. Added output

--- main.py
+++ main.py
@@ -8,6 +8,26 @@ def log(values):
 		print(values)
 
 
+
+def ones(arr: np.ndarray):
+	# Find the indices where value is 1
+	y_indices, x_indices = np.where(arr == 1)
+
+	# Filter the indices based on the constraint
+	valid_indices = [(x, y) for x, y in zip(x_indices, y_indices) if arr[0][x] == 0]
+	# x+1 - номер икса, y - строчка с его ответом
+	return valid_indices
+
+def output(tableau: np.ndarray):
+	valid_indices = ones(tableau)
+	n,m = tableau.shape
+	x = np.zeros(m-n)
+	for item in valid_indices:
+		x[item[0]] = tableau[item[1]][m-1]
+	print("A vector of decision variables:", x)
+	print("Optimal value:",tableau[0][tableau.shape[1]-1])
+
+
 def maximize(A: np.ndarray, b: np.ndarray, c: np.ndarray):
 	assert np.ndim(A) == 2
 	m, n = A.shape
@@ -23,17 +43,18 @@ def maximize(A: np.ndarray, b: np.ndarray, c: np.ndarray):
 
 	i = 0
 	while True:
-		log(f"Iteration #{i} tableau:\n{tableau}")
+		# log(f"Iteration #{i} tableau:\n{tableau}")
 		z_row = tableau[0, :n]
 
 		pivot_n = np.argmin(z_row)  # Determine pivot column index
 		pivot = z_row[pivot_n]
 
 		if pivot >= 0:
-			log("No more pivot columns available, can't optimize")
+			# log("No more pivot columns available, can't optimize")
+			output(tableau)
 			break  # Can not optimize more
 
-		log(f"Pivot column available: {pivot_n}")
+		# log(f"Pivot column available: {pivot_n}")
 		pivot_col = tableau[1:, pivot_n]
 		b_col = tableau[1:, -1]
 
@@ -41,14 +62,16 @@ def maximize(A: np.ndarray, b: np.ndarray, c: np.ndarray):
 		positive_indices = np.where(ratio > 0)  # Indices of positive ratios
 
 		if not positive_indices:
-			log("No more pivot rows available, can't optimize")
+			# log("No more pivot rows available, can't optimize")
+			output(tableau)
 			break
 
-		pivot_m = np.argmin(ratio[positive_indices]) + 1
-		log(f"Pivot row available: {pivot_m}")
+		index = np.argmin(ratio[positive_indices])
+		pivot_m = positive_indices[0][index] +1
+		# log(f"Pivot row available: {pivot_m}")
 
 		pivot = tableau[pivot_m, pivot_n]
-		log(f"Pivot: {pivot:.2f} at ({pivot_m}, {pivot_n})")
+		# log(f"Pivot: {pivot:.2f} at ({pivot_m}, {pivot_n})")
 		tableau[pivot_m] /= pivot
 
 		# Row elimination
@@ -58,24 +81,29 @@ def maximize(A: np.ndarray, b: np.ndarray, c: np.ndarray):
 
 			tableau[row] -= tableau[pivot_m] * tableau[row, pivot_n]
 
-		log('')
+		# log('')
 		i += 1
 
 
-file_info = np.loadtxt("input.txt", dtype=float, delimiter=',', skiprows=1)
+
+file_info = np.genfromtxt("input.txt", delimiter=',', skip_footer=2)
+
 c = file_info[0]
 b = file_info[file_info.shape[0]-1]
 a = file_info[1:file_info.shape[0]-1]
+x = a[~np.isnan(a)] # Get rid of nan values
+b = b[~np.isnan(b)] # Get rid of nan values
 
 with open("input.txt") as file:
-	line = file.readline().split(",")
+	lines = file.readlines()
+	task_type = lines[len(lines)-1]
+	accuracy = lines[len(lines)-3]
+	decimals = len(accuracy.split('.')[1])-1
 
-task_type = line[0]
-accuracy = len(line[1].split(".")[1])-1
-
+# Change minimize problem to maximize
 c = -c if task_type == "Minimize" else c
 
-np.set_printoptions(precision=accuracy, suppress=True)
+np.set_printoptions(precision=decimals, suppress=True)
 
 if np.any(b < 0) :
 	log('The method is not applicable!')
