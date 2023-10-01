f00d91c723e089e903a72f751351b4dd18bea95c
Timofey <timofey.brayko@gmail.com>
2023-10-01 15:10:41 +0300

    Fixed input and output. Ignoring division by zero error message

--- main.py
+++ main.py
@@ -11,21 +11,20 @@ def log(values):
 
 def ones(arr: np.ndarray):
 	# Find the indices where value is 1
-	y_indices, x_indices = np.where(arr == 1)
+	y_indices, x_indices = np.where(arr[0:, :abs(arr.shape[1]-arr.shape[0]) ] == 1)
 
-	# Filter the indices based on the constraint
+	# Filter the indices
 	valid_indices = [(x, y) for x, y in zip(x_indices, y_indices) if arr[0][x] == 0]
-	# x+1 - номер икса, y - строчка с его ответом
 	return valid_indices
 
 def output(tableau: np.ndarray):
 	valid_indices = ones(tableau)
 	n,m = tableau.shape
-	x = np.zeros(m-n)
+	x = np.zeros(abs(m-n))
 	for item in valid_indices:
 		x[item[0]] = tableau[item[1]][m-1]
 	print("A vector of decision variables:", x)
-	print("Optimal value:",tableau[0][tableau.shape[1]-1])
+	print("Optimal value:", np.array([tableau[0][tableau.shape[1]-1]]))
 
 
 def maximize(A: np.ndarray, b: np.ndarray, c: np.ndarray):
@@ -58,7 +57,8 @@ def maximize(A: np.ndarray, b: np.ndarray, c: np.ndarray):
 		pivot_col = tableau[1:, pivot_n]
 		b_col = tableau[1:, -1]
 
-		ratio = b_col / pivot_col # FIXME: division by zero
+		with np.errstate(divide='ignore'):
+			ratio = b_col / pivot_col # FIXME: division by zero
 		positive_indices = np.where(ratio > 0)  # Indices of positive ratios
 
 		if not positive_indices:
@@ -85,20 +85,15 @@ def maximize(A: np.ndarray, b: np.ndarray, c: np.ndarray):
 		i += 1
 
 
-
-file_info = np.genfromtxt("input.txt", delimiter=',', skip_footer=2)
-
-c = file_info[0]
-b = file_info[file_info.shape[0]-1]
-a = file_info[1:file_info.shape[0]-1]
-x = a[~np.isnan(a)] # Get rid of nan values
-b = b[~np.isnan(b)] # Get rid of nan values
-
 with open("input.txt") as file:
-	lines = file.readlines()
-	task_type = lines[len(lines)-1]
-	accuracy = lines[len(lines)-3]
-	decimals = len(accuracy.split('.')[1])-1
+	elements = file.read().replace("\n",' ').split("#")[1:]
+c, a, b, accuracy, task_type = [np.asarray(i.split(":")[1].strip().split(" ")) for i in elements]
+c = np.array(c, dtype=float)
+b = np.array(b, dtype=float)
+a = np.reshape(np.array(a, dtype=float), (-1,c.size))
+
+decimals = len(str(accuracy[0]).split('.')[1])
+
 
 # Change minimize problem to maximize
 c = -c if task_type == "Minimize" else c
